7a8072bd96bb98e952bcf156b14d95ebcdc64dd6
medoum <medoum2430@gmail.com>
2023-02-13 17:08:24 +0100

    first commit

---
+++ ApiTest/Models/UserLogins.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTest.Models
+{
+	public class UserLogins
+	{
+		public required String Username { get; set; }
+
+		public required String Password { get; set; }
+	}
+}
+
---
+++ ApiTest/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiTest.Models
+{
+	public class UserModel
+	{
+		public required string Username { get; set; }
+
+		public string Password { get; set; }
+
+		public required string EmailAddress { get; set; }
+
+		public required string Role { get; set; }
+
+		public required string Surname { get; set; }
+
+		public required string GivenName { get; set; }
+	}
+}
+
